10de41b4ccd0c6d96ff0fa63c6956918fa971cb0
Israel Portillo <81596661+Z4TUR@users.noreply.github.com>
2024-03-26 17:34:01 -0600

    Update Collatz_Conjecture.py
    Reducción de codigo

--- Collatz_Conjecture.py
+++ Collatz_Conjecture.py
@@ -1,17 +1,14 @@
 def steps(number:int):
     if number < 1:
         raise ValueError("Only positive integers are allowed")
-    pasos = []
     stps = 0
     for n in range(1, number):
         if number == 1:
-            nu = int(number)
+            number = number
             break 
         elif number%2 == 0:
-            nu = int(number/2)
+            number = number//2
         else:
-            nu = int(3*number+1)
-        number = nu
+            number = 3*number+1
         stps += 1
-
     return stps
